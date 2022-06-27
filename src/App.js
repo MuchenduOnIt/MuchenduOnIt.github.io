73692e713c06a2bceccddd9abe54c3c61ab72089
Stephen Muchendu <muchendumaina66@gmail.com>
2022-06-28 00:00:49 +0300

    NavBar not working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Background from './animated/animated';
+import Navbar from './components/nav';
 import './index.css';
 
 function App() {
   return (
     <div className="App">
       <Background className="background" />
+      <Navbar className="navBar"/>
       <div className="hello">
         Hello there <span className="wave">👋🏾</span> !
       </div>
---
+++ src/components/nav.jsx
@@ -0,0 +1,9 @@
+import styles from './nav.module.css'
+
+export default function Navbar() {
+    return (
+        <>
+            <p>This is a nav bar</p>
+        </>
+    )
+}
