b8079b00368c9cc38c80c855064ccd577fc28673
Stephen Muchendu <muchendumaina66@gmail.com>
2022-06-27 22:49:26 +0300

    Animated Background

---
+++ src/animated/animated.jsx
@@ -0,0 +1,26 @@
+import styles from '.animated.module.css';
+
+export default function animatedBackground() {
+  return (
+    <>
+          <div className={styles.context}>
+        <h1>Pure Css Animated Background</h1>
+      </div>
+
+          <div className={ styles.area}>
+              <ul className={styles.circles}>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </>
+  );
+}
